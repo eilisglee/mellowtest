d9822937719ac8a6d1af8b5174678d32803b6fdc
eilisglee <elee2@seattleu.edu>
2020-07-25 15:05:51 -0700

    add main stylesheet

--- mellowtheme/functions.php
+++ mellowtheme/functions.php
@@ -6,6 +6,9 @@ function load_css()
 {
     wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
+
+    wp_register_style('main', get_template_directory_uri() . '/css/maincss', array(), false, 'all');
+    wp_enqueue_style('main');
 }
 add_action('wp_enqueue_scripts', 'load_css');
 
