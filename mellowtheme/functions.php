0e109f47b89fec602a797e11b3f77ac0da534164
eilisglee <elee2@seattleu.edu>
2020-07-25 19:08:11 -0700

    add footer menu

--- mellowtheme/functions.php
+++ mellowtheme/functions.php
@@ -28,6 +28,7 @@ add_theme_support('menus');
 register_nav_menus(
     array(
         'top-menu' => 'Top Menu Location',
-        'mobile-menu' => 'Mobile Menu Location'
+        'mobile-menu' => 'Mobile Menu Location',
+        'footer-menu' => 'Footer Menu Location'
     )
 );
